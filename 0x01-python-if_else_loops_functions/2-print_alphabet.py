c1f92a79913de1a5aad88c734c11c9ab8de12d68
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-03 00:26:55 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(26):
-    print("{}".format(chr(97 + i)), end = "")
+    print("{:c}".format(i), end='')
+
+# for i in range(26):
+#     print("{}".format(chr(97 + i)), end = "")
     
 # for i in range(26):
 #     letter = chr(97 + i)
