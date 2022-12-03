cf093ad4c4d798bcfffb38a0a56ac82f238409e9
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-03 14:34:55 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     """converts letters to uppercase"""
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 122:
-              letter = chr(ord(letter) - 32)
+                letter = chr(ord(letter) - 32)
         print("{:s}".format(letter), end="")
     print("")
