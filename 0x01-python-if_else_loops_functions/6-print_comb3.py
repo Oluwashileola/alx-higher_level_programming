f98aeb0cba6ee0b29edb04064fe70a2b53e612b0
Akindoju Shileola <spickbliss@gmail.com>
2022-12-03 02:28:13 -0800

    added permission

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for num1 in range(0, 10):
         if num1 == 8 and num2 == 9:
             print("{}{}".format(num1, num2))
         else:
-            print("{}{}".format(num1, num2), end = ", ")
+            print("{}{}".format(num1, num2), end=", ")
