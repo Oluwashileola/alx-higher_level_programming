e4c1fac24c48a087a8a1105e9a3dc6ec5f28d811
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-03 09:59:07 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print("{}".format(number), end='')
+        print("{}".format(number))
     else:
-        print("{}".format(number), end=', ')
+        print("{:02}".format(number), end=", ")
 
 #Write a program that prints numbers from 0 to 99 seperated by a comma.
