7f6896e4745d39a89f2fad508871ac292cf1aa0c
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-04 10:59:58 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Write a program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase
+'''Write a program that prints the ASCII alphabet,
+in reverse order, alternating lowercase and uppercase'''
 
-ecrement = 0
+decrement = 0
 for i in range(-122, -96):
     if abs(i) % 2 == 0:
         decrement = 0
