787ac176901c53183e6d9f39e470ef0eefa9383f
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-10 16:31:02 +0100

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    num = len(sys.argv)
+    if num == 0:
+        print("{} arguments.")
+    elif num == 1:
+        print("{} argument:".format(num))
+    else:
+        print("{} arguments:".format(num))
+
+    for i in range(0, num):
+        print("{}: {}".format(i + 1, sys.argv[i]))
