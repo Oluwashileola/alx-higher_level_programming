5fa610aff3705732dcf8237c3dfd150ddbdb610a
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-12 12:48:37 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,32 +2,32 @@
 
 if __name__ == "__main__":
     '''adds, subtracts, multiplies and divides two numbers'''
-    import calculator_1, sys
+    from calculator_1 import add, sub, mul, div
+    import sys
 
     arg_error_message = "Usage: ./100-my_calculator.py <a> <operator> <b>"
     operator_error_message = "Unknown operator. Available operators: +, -, * and /"
 
-
-    if (len(sys.argv) - 1)!= 3:
+    if (len(sys.argv) - 1) != 3:
         print("{}".format(arg_error_message))
-        print(1)
+        sys.exit(1)
     else:
         a = int(sys.argv[0 + 1])
         b = int(sys.argv[2 + 1])
         operator = sys.argv[1 + 1]
 
         if operator == "+":
-            print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-            print(0)
+            print("{} + {} = {}".format(a, b, add(a, b)))
+            sys.exit(0)
         elif operator == "-":
-            print("{} - {} = {}".format(a, b, calculator_1.sub(a, b))
-            print("{}".format(0))
+            print("{} - {} = {}".format(a, b, sub(a, b))
+            sys.exit(0)
         elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(a, b, calculator_1.mul(a, b))
+            print("{} * {} = {}".format(a, b, mul(a, b))
             print(0)
         elif operator == "/":
-            print("{} / {} = {}".format(a, b, calculator_1.div(a, b))
-            print(0)
+            print("{} / {} = {}".format(a, b, div(a, b))
+            sys.exit(0)
         else:
             print("{}".format(operator_error_message))
-            print(1)
+            sys.exit(1)
