710112d6078756b575c91465287fd452ca2374cb
Oluwashileola <111228750+Oluwashileola@users.noreply.github.com>
2022-12-23 00:09:20 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for x in range(len(matrix[i])):
             print("{}".format(matrix[i][x]), end=" ")
-            if x != len(matrix[i]):
-                print(" ", end="")
         print("")
